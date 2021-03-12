0b13b36ec5125c54b7b53ea6988e36494f56c546
Kinomancer <luisramos99@aragon.unam.mx>
2021-03-11 19:19:28 -0600

    Tabla de Multiplicar Python

---
+++ TablaMultiplicar.py
@@ -0,0 +1,40 @@
+## Ingresa el número con el que se realizarán las operaciones de la tabla
+## convierte el string en entero y detiene el programa si no encuentra un entero positivo
+tabla = input("¿Que número desea multiplicar?")
+try:
+    t = int(tabla)
+    if t <0:
+        quit()
+except:
+    print("Favor de ingresar un número positivo")
+    quit()
+
+## Ingresa donde se iniciará la tabla de multiplicar
+## convierte el string en entero y detiene el programa si no encuentra un entero positivo
+
+start = input("¿Desde donde desea que inicie la tabla?")
+
+try:
+    s = int(start)
+    if s <0:
+        quit()
+except:
+    print("Favor de ingresar un número positivo")
+    quit()
+## Ingresa donde se terminará la tabla de multiplicar
+## convierte el string en entero y detiene el programa si no encuentra un entero positivo
+
+end = input("¿Hasta donde desea que termine la tabla?")
+
+try:
+    e = int(end)
+    if e < s:
+        quit()
+except:
+    print("Favor de ingresar un número mayor al inicio")
+    quit()
+
+## Generación de un contador sencillo que imprime las tablas de acuerdo a las entradas anteriores
+while s <= e:
+    print(tabla," * ", s, " = ", t*s)
+    s += 1
